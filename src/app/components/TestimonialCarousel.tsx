a424d1c8f194ae0e3d82c0df8199125ee6355f2d
kensikaeel <kensikaeel@gmail.com>
2026-02-26 07:17:24 +0000

    make testimonial card dynamically

--- src/app/components/TestimonialCarousel.tsx
+++ src/app/components/TestimonialCarousel.tsx
@@ -15,27 +15,27 @@ import { testimonials } from './TestimonialData.tsx';
 
 export function TestimonialCarousel() {
   const plugin = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: true })
+    Autoplay({ delay: 2000, stopOnInteraction: false })
   );
 
   return (
-    <Carousel
-      plugins={[plugin.current]}
-      className="w-full max-w-xs sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl mx-auto"
-      onMouseEnter={plugin.current.stop}
-      onMouseLeave={plugin.current.reset}
-    >
-      <CarouselContent>
-        {testimonials.map((testimonial) => (
-          <CarouselItem key={testimonial.id} className="sm:basis-1/2 lg:basis-1/3">
-            <div className="p-2 h-full">
-              <TestimonialCard testimonial={testimonial} />
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className="hidden sm:flex" />
-      <CarouselNext className="hidden sm:flex" />
-    </Carousel>
-  );
+      <Carousel
+        plugins={[plugin.current]}
+        className="w-full max-w-xs sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl mx-auto"
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+      >
+        <CarouselContent>
+          {testimonials.map((testimonial) => (
+            <CarouselItem key={testimonial.id} className="sm:basis-1/2 lg:basis-1/3">
+              <div className="p-2 h-full">
+                <TestimonialCard testimonial={testimonial} />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="hidden sm:flex" />
+        <CarouselNext className="hidden sm:flex" />
+      </Carousel>
+    );
 }
